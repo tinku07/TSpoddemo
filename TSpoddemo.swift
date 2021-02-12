3d53bc2972f297051b2aacd3f247347957408ab4
CodeBuzzers <codebuzzers@CodeBuzzerss-MacBook-Pro.local>
2021-02-12 17:15:13 +0530

    firtst commit

---
+++ TSpoddemo.swift
@@ -0,0 +1,20 @@
+//
+//  TSpoddemo.swift
+//  TSpoddemo
+//
+//  Created by CodeBuzzers on 12/02/21.
+//
+
+import UIKit
+
+class TSpoddemo: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
