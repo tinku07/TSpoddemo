cd2062fa28952332fe3881e419067bfd6cea8884
CodeBuzzers <codebuzzers@CodeBuzzerss-MacBook-Pro.local>
2021-02-15 13:41:40 +0530

    add func

---
+++ TSpoddemo/TSpoddemo.swift
@@ -0,0 +1,24 @@
+//
+//  TSpoddemo.swift
+//  TSpoddemo
+//
+//  Created by CodeBuzzers on 15/02/21.
+//
+
+public final class TSpoddemo {
+
+    let name = "TSpoddemoLib"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+
+    
+    
+    
+    
+}
--- TSpoddemo/ViewController.swift
+++ TSpoddemo/ViewController.swift
@@ -7,7 +7,8 @@
 
 import UIKit
 class ViewController: UIViewController {
-
+    var obj = TSpoddemo()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -15,6 +16,10 @@ class ViewController: UIViewController {
         let TView = TSpopddemoView(frame: self.view.bounds)
                 
         self.view.addSubview(TView)
+        
+        let result  = obj.add(a: 1, b: 2)
+        debugPrint("\(result)")
+        
     }
 
 
